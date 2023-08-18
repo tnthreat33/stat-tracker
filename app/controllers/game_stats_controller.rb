550be58803967ab25a34d85cefe1b0473cbbf440
Rachel Hamby <tnthreat33@gmail.com>
2023-08-17 20:10:37 -0400

    add destroy route and controller action

--- app/controllers/game_stats_controller.rb
+++ app/controllers/game_stats_controller.rb
@@ -14,11 +14,29 @@ class GameStatsController < ApplicationController
         end
     end 
 
+    def destroy 
+        if @game_stat
+        #   if @reservation.user_id == @current_user.id
+            @reservation.destroy 
+            head :no_content
+        #   else 
+        #     render json: { error: "Unauthorized: Cannot delete reservation" }, status: :unauthorized
+        #   end 
+        else 
+          render json: { error: "Reservation not found" }, status: :not_found
+        end 
+      end
+
+
     private
 
 def stat_params
     params.permit(:game_id, :player_id, :played, :batting_average, :at_bat, :hits, :runs, :RBI, :stolen_base, :field_error, :fielding_percentage, :innings_pitched, :ERA, :K )
 end
+
+def find_stat
+    @game_stat = GameStat.find_by(id: params[:id])
+  end
 end
 
    
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :teams, only: [:index]
-  resources :game_stats, only: [:index, :create]
+  resources :game_stats, only: [:index, :create, :destroy]
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
