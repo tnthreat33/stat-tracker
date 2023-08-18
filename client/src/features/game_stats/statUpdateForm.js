d8f22ed7459e1d31c5ad88f083c3225f246fce6d
Rachel Hamby <tnthreat33@gmail.com>
2023-08-18 13:52:51 -0400

    add form for game stat update

--- client/src/features/game_stats/statUpdateForm.js
+++ client/src/features/game_stats/statUpdateForm.js
@@ -1,23 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import { updateGameStat } from "./statSlice";
+import { updateGameStat, fetchStats } from "./statSlice";
+
 
 
 function StatUpdateForm() {
   const { statId } = useParams();
   const dispatch = useDispatch();
   const stats = useSelector((state) => state.stats.entities);
-  const statToUpdate = stats.find((stat) => stat.id === statId);
+  const statToUpdate = stats.find((stat) => stat.id === parseInt(statId));
 
-  console.log(stats)
+  useEffect(() => {
+    dispatch(fetchStats());
+  }, [dispatch]);
 
   const [updatedStat, setUpdatedStat] = useState({
-    // Initialize the form fields with the current data
+    game_id: statToUpdate.game_id,
+    player_id: statToUpdate.player_id,
+    played: 1,
     ERA: statToUpdate.ERA,
     K: statToUpdate.K,
     RBI: statToUpdate.RBI,
-    // ... other fields
+    fielding_percentage: statToUpdate.fielding_percentage,
+    batting_average:statToUpdate.batting_average,
+    at_bat: statToUpdate.at_bat,
+    fielding_error: statToUpdate.fielding_error,
+    hits: statToUpdate.hits,
+    innings_pitched: statToUpdate.innings_pitched,
+    runs: statToUpdate.runs,
+    stolen_base: statToUpdate.stolen_base
   });
 
   const handleInputChange = (event) => {
@@ -34,6 +46,17 @@ function StatUpdateForm() {
     // Redirect or handle success as needed
   };
 
+  const availableGames = [...new Map(stats.map(stat => [stat.game_id, stat])).values()];
+    
+
+    const availablePlayers = useSelector((state) => {
+      const players = [];
+      state.teams.entities.forEach((team) => {
+        players.push(...team.players); // Assuming each team has a "players" property
+      });
+      return players;
+    });
+
   return (
     <div>
       <h2>Edit Stat</h2>
@@ -46,7 +69,46 @@ function StatUpdateForm() {
           value={updatedStat.ERA}
           onChange={handleInputChange}
         />
-        {/* Other input fields */}
+        <label>K:</label>
+        <input type="number" value={statToUpdate.K} onChange={handleInputChange} />
+        <label>RBI:</label>
+        <input type="number" value={statToUpdate.RBI} onChange={handleInputChange} />
+        <label>AB:</label>
+        <input type="number" value={statToUpdate.at_bat} onChange={handleInputChange} />
+        <label>BA:</label>
+        <input type="number" step="0.01" value={statToUpdate.batting_average} onChange={handleInputChange}/>
+        <label>Errors:</label>
+        <input type="number" value={statToUpdate.fielding_error} onChange={handleInputChange}/>
+        <label>Fielding %:</label>
+        <input type="number" value={statToUpdate.fielding_percentage} onChange={handleInputChange} />
+        <label>Hits:</label>
+        <input type="number" value={statToUpdate.hits} onChange={handleInputChange} />
+        <label>IP:</label>
+        <input type="number" value={statToUpdate.innings_pitched} onChange={handleInputChange} />
+        <label>Runs:</label>
+        <input type="number" value={statToUpdate.runs} onChange={handleInputChange} />
+        <label>Stolen Bases:</label>
+        <input type="number" value={statToUpdate.stolen_base} onChange={handleInputChange}/>
+
+        <label>Game:</label>
+        <select name="game_id" value={updatedStat.game_id} onChange={handleInputChange}>
+          <option value="">Select an option</option>
+          {availableGames.map((option) => (
+            <option key={option.id} value={option.id}>
+              {option.game.away_team_name} vs. {option.game.home_team_name}
+            </option>
+          ))}
+        </select>
+
+        <label>Player:</label>
+        <select name="player_id" value={updatedStat.player_id} onChange={handleInputChange}>
+          <option value="">Select an option</option>
+          {availablePlayers.map((option) => (
+            <option key={option.id} value={option.id}>
+              {option.name}
+            </option>
+          ))}
+        </select>
         <button type="submit">Update</button>
       </form>
     </div>
