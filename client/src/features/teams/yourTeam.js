836e70d80485ad03e2646a8471dc9bcdb22712bd
Rachel Hamby <tnthreat33@gmail.com>
2023-08-29 20:30:27 -0400

    fix your team display

--- client/src/features/teams/yourTeam.js
+++ client/src/features/teams/yourTeam.js
@@ -1,29 +1,29 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchUserTeam } from "./teamsSlice";
+import { fetchUserTeam } from "./teamsSlice"; 
 
-function YourTeam (){
-    const dispatch = useDispatch();
-    const userId = useSelector((state) => state.auth.user);
-    const userTeam = useSelector((state) => state.teams.userTeam);
-    console.log(userId)
+function YourTeam() {
+  const dispatch = useDispatch();
+  const userId = useSelector((state) => state.auth.user);
+  const userTeams = useSelector((state) => state.teams.userTeam); // Assuming this is an array
 
-    useEffect(() => {
-        dispatch(fetchUserTeam(userId.id));
-      }, [dispatch, userId]);
-    
-      if (!userTeam) {
-        return <p>Loading...</p>;
-      }
+  useEffect(() => {
+    dispatch(fetchUserTeam(userId));
+  }, [dispatch, userId]);
 
-      console.log(userTeam)
-    return(
-        <div>
-          <h1>Team Details</h1>
+  if (!userTeams || userTeams.length === 0) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h1>Your Teams</h1>
+      {userTeams.map((userTeam) => (
+        <div key={userTeam.id}>
           <h2>{userTeam.name} - {userTeam.nickname}</h2>
           <p>Address: {userTeam.address}</p>
           <p>Wins: {userTeam.wins} - Losses: {userTeam.losses}</p>
-    
+
           <h3>Players</h3>
           <table>
             <thead>
@@ -34,18 +34,19 @@ function YourTeam (){
               </tr>
             </thead>
             <tbody>
-              {/* {userTeam.players.map((player) => (
+              {userTeam.players.map((player) => (
                 <tr key={player.id}>
                   <td>{player.jersey_number}</td>
                   <td>{player.name}</td>
                   <td>{player.position}</td>
-                </tr> */}
-              {/* ))} */}
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
-      );
-    }
-
+      ))}
+    </div>
+  );
+}
 
 export default YourTeam;
